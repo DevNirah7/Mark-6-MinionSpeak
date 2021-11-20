1cc2bfe2070a3ec8a6c1314a9c51d451900a89c0
Harin <89265161+DevNirah7@users.noreply.github.com>
2021-11-20 14:03:55 +0530

    Update: Comments removal

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function getTranslationURL(text) {
     return serverURL + "?" + "text=" + text
 
 }
-//error handler function
+
 function errorHandler(error) {
     console.log("error occured", error);
     alert("something wrong with server! try again after some time")
